3461198defbe3878d95a04c0ef00871c8e3afca6
Alyssa Biasi <biasi91@gmail.com>
2019-05-29 16:09:43 +1000

    Ex 5.2 5.3

--- src/chp5/Stream.scala
+++ src/chp5/Stream.scala
@@ -10,6 +10,28 @@ sealed trait Stream[+A] {
 
     go(this, List()).reverse
   }
+
+  def take(n: Int): Stream[A] = {
+    this match {
+      case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n-1))
+      case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
+      case _ => Stream.empty
+    }
+  }
+
+  def drop(n: Int): Stream[A] = {
+    this match {
+      case Cons(_, t) if n > 1 => t().drop(n - 1)
+      case _ => this
+    }
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    this match {
+      case Cons(h, t) if p(h()) => Stream.cons(h(), t().takeWhile(p))
+      case _ => Stream.empty
+    }
+  }
 }
 
 case object Empty extends Stream[Nothing]
--- src/chp5/chp5-streams.sc
+++ src/chp5/chp5-streams.sc
@@ -1,2 +1,8 @@
 
 Stream.cons(1, Stream.empty).toList
+
+Stream.apply(1,2,3,4,5,6).take(4).toList
+
+Stream.apply(1,2,3,4,5,6).drop(4).toList
+
+Stream.apply(2,4,6,7,8,9).takeWhile(_ % 2 == 0).toList
