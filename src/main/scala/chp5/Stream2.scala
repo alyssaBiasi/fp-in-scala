ef9d8b5b5f72f5c3ff327e3cf5abeb97bf2b98d4
Alyssa Biasi <biasi91@gmail.com>
2019-05-30 16:25:34 +1000

    Ex 5.13

--- src/main/scala/chp5/Stream2.scala
+++ src/main/scala/chp5/Stream2.scala
@@ -26,6 +26,14 @@ sealed trait Stream2[+A] {
     }
   }
 
+  def takeWithUnfold(n: Int): Stream2[A] = {
+    Stream2.unfold((this, n)) {
+      case (Cons(h, t), x) if x > 1 => Some((h(), (t().take(x-1), x-1)))
+      case (Cons(h, _), x) if x == 1 => Some((h(), (Stream2.empty, x-1)))
+      case _ => None
+    }
+  }
+
   def drop(n: Int): Stream2[A] = {
     this match {
       case Cons(_, t) if n > 1 => t().drop(n - 1)
@@ -44,6 +52,13 @@ sealed trait Stream2[+A] {
     foldRight(Stream2.empty[A])(
       (h, t) => if (f(h)) Stream2.cons(h, t) else Stream2.empty
     )
+  }
+
+  def takeWhileWithUnfold(p: A => Boolean): Stream2[A] = {
+    Stream2.unfold[A, Stream2[A]](this) {
+      case Cons(h, t) if p(h()) => Some((h(), t()))
+      case _ => None
+    }
   }
 
   def exists(p: A => Boolean): Boolean = {
@@ -66,6 +81,13 @@ sealed trait Stream2[+A] {
     )
   }
 
+  def mapWithUnfold[B](f: A => B): Stream2[B] = {
+    Stream2.unfold[B, Stream2[A]](this) {
+      case Cons(h, t) => Some((f(h()), t()))
+      case _ => None
+    }
+  }
+
   def filter(p: A => Boolean): Stream2[A] = {
     foldRight[Stream2[A]](Stream2.empty)(
       (h, t) => if (p(h)) Stream2.cons(h, t) else t
@@ -80,6 +102,24 @@ sealed trait Stream2[+A] {
     foldRight[Stream2[B]](Stream2.empty)(
       (h, t) => f(h).append(t)
     )
+  }
+
+  def zipAll[B](s2: Stream2[B]): Stream2[(Option[A], Option[B])] = {
+    Stream2.unfold((this, s2)) {
+      case (Cons(h1,t1), Cons(h2, t2)) => Some((
+        (Some(h1()), Some(h2())),
+        (t1(), t2())
+      ))
+      case (Cons(h1,t1), Empty) => Some((
+        (Some(h1()), None),
+        (t1(), Stream2.empty)
+      ))
+      case (Empty, Cons(h2, t2)) => Some((
+        (None, Some(h2())),
+        (Stream2.empty, t2())
+      ))
+      case (Empty, Empty) => None
+    }
   }
 }
 
--- src/test/scala/chp5/Stream2Spec.scala
+++ src/test/scala/chp5/Stream2Spec.scala
@@ -10,9 +10,18 @@ class Stream2Spec extends Specification {
       result must containTheSameElementsAs(List(1))
     }
 
-    "take" in {
-      val result = Stream2.apply(1,2,3,4,5,6).take(4).toList
-      result must containTheSameElementsAs(List(1, 2, 3, 4))
+    "take" should {
+      "should return elements" in {
+        val result = Stream2.apply(1,2,3,4,5,6).take(4).toList
+        result must containTheSameElementsAs(List(1, 2, 3, 4))
+      }
+
+      "with unfold" should {
+        "should return elements" in {
+          val result = Stream2.apply(1,2,3,4,5,6).takeWithUnfold(4).toList
+          result must containTheSameElementsAs(List(1, 2, 3, 4))
+        }
+      }
     }
 
     "drop" in {
@@ -20,9 +29,18 @@ class Stream2Spec extends Specification {
       result must containTheSameElementsAs(List(4,5,6))
     }
 
-    "takeWhile" in {
-      val result = Stream2.apply(2,4,6,7,8,9).takeWhile(_ % 2 == 0).toList
-      result must containTheSameElementsAs(List(2, 4, 6))
+    "takeWhile" should {
+      "should return elements" in {
+        val result = Stream2.apply(2, 4, 6, 7, 8, 9).takeWhile(_ % 2 == 0).toList
+        result must containTheSameElementsAs(List(2, 4, 6))
+      }
+
+      "with unfold" should {
+        "should return elements" in {
+          val result = Stream2.apply(2, 4, 6, 7, 8, 9).takeWhileWithUnfold(_ % 2 == 0).toList
+          result must containTheSameElementsAs(List(2, 4, 6))
+        }
+      }
     }
 
     "takeWhileWithFold" in {
@@ -65,6 +83,13 @@ class Stream2Spec extends Specification {
         val result = Stream2.apply(1,2,3).map(_ + 1)
         result.toList must containTheSameElementsAs(List(2,3,4))
       }
+
+      "with unfold" should {
+        "return transformed stream" in {
+          val result = Stream2.apply(1,2,3).mapWithUnfold(_ + 1)
+          result.toList must containTheSameElementsAs(List(2,3,4))
+        }
+      }
     }
 
     "filter" should {
@@ -94,6 +119,32 @@ class Stream2Spec extends Specification {
 
         val result = stream.take(5)
         result.toList must containTheSameElementsAs(List(0,1,2,3,4))
+      }
+    }
+
+    "zipAll" should {
+      "return combined streams" in {
+        val stream1 = Stream2.apply(1,2,3)
+        val result = stream1.zipAll(stream1)
+
+        val expected = List(
+          (Some(1), Some(1)),
+          (Some(2), Some(2)),
+          (Some(3), Some(3))
+        )
+        result.toList must containTheSameElementsAs(expected)
+      }
+
+      "return combined streams of different length" in {
+        val stream1 = Stream2.apply(1,2,3)
+        val result = stream1.zipAll(stream1.take(1))
+
+        val expected = List(
+          (Some(1), Some(1)),
+          (Some(2), None),
+          (Some(3), None)
+        )
+        result.toList must containTheSameElementsAs(expected)
       }
     }
 
