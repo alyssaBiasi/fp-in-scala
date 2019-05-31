4bf96d8f6386bd2d3c1d495c9051fe662e083f32
Alyssa Biasi <biasi91@gmail.com>
2019-05-31 15:38:33 +1000

    Ex 6.10
    State

---
+++ src/main/scala/chp6/State.scala
@@ -0,0 +1,32 @@
+package chp6
+
+case class State[S, +A](run: S => (A,S)) {
+  def flatMap[B](f: A => State[S, B]): State[S,B] = {
+    State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
+  }
+
+  def map[B](f: A => B): State[S,B] = {
+    flatMap(a => State.unit(f(a)))
+  }
+
+  def map2[B,C](rb: State[S,B])(f: (A,B) => C): State[S,C] = {
+    flatMap(
+      a => rb.map(b => f(a,b))
+    )
+  }
+}
+
+object State {
+  def unit[S, A](a: A): State[S, A] = {
+    State(s => (a, s))
+  }
+
+  def sequence[S,A](fs: List[State[S,A]]): State[S,List[A]] = {
+    fs.foldRight[State[S,List[A]]](unit(Nil))(
+      (s, acc) => s.map2(acc)(_ :: _)
+    )
+  }
+}
